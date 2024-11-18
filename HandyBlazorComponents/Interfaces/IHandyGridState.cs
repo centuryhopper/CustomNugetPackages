366956ce989e282d9bf0af301f9fc9e450415de7
centuryhopper <43508492+centuryhopper@users.noreply.github.com>
2024-11-18 10:43:06 -0500

    update

--- HandyBlazorComponents/Interfaces/IHandyGridState.cs
+++ HandyBlazorComponents/Interfaces/IHandyGridState.cs
@@ -7,13 +7,12 @@ namespace HandyBlazorComponents.Interfaces;
 public interface IHandyGridState<T, U> where T : HandyGridEntityAbstract<U> where U : class, new()
 {
     List<T> Items { get; set; }
+    List<string> Columns => typeof(U).GetProperties().Select(prop => prop.Name).ToList();
     GridValidationResponse ValidationChecks(T item, List<string> columns);
     Dictionary<string, RenderFragment<T>> EditModeFragments { get; set; }
     Dictionary<string, RenderFragment<T>> ViewModeFragments { get; set; }
     IReadOnlyCollection<string> ReadonlyColumns { get; set; }
     string ExampleFileUploadUrl { get; set; }
-
-    EventCallback<IEnumerable<T>> SubmitFileAction { get =>
-        EventCallback.Factory.Create<IEnumerable<T>>(this, OnSubmitFile); }
+    EventCallback<IEnumerable<T>> SubmitFileAction => EventCallback.Factory.Create<IEnumerable<T>>(this, OnSubmitFile);
     Func<IEnumerable<T>, Task> OnSubmitFile {get;set;}
 }
