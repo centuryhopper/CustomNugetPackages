e805928ecfc113aab06607225773ec11c6f861fd
centuryhopper <43508492+centuryhopper@users.noreply.github.com>
2025-03-23 17:03:54 -0400

    update

---
+++ HandyBlazorComponents/ComponentBases/ModalBase.cs
@@ -0,0 +1,63 @@
+using HandyBlazorComponents.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace HandyBlazorComponents.ComponentBases;
+
+public class ModalBase : ComponentBase
+{
+    protected bool IsVisible { get; set; }
+    protected TaskCompletionSource<bool>? TaskCompletionSource;
+    protected string ModalClass = string.Empty;
+    [Parameter] public string Title { get; set; } = "Confirmation";
+    [Parameter] public string BodyText { get; set; } = "Are you sure?";
+    [Parameter] public HandyModalType HandyModalType { get; set; } = HandyModalType.INFO;
+    public virtual Task<bool> ShowAsync(string title, string bodyText, HandyModalType chosenModalType)
+    {
+        // prioritize user passed in parameters over component parameters
+        HandyModalType = chosenModalType;
+        Title = title;
+        BodyText = bodyText;
+        SetModalClass(HandyModalType);
+        IsVisible = true;
+        TaskCompletionSource = new TaskCompletionSource<bool>();
+        StateHasChanged();
+        return TaskCompletionSource.Task;
+    }
+
+    public virtual Task<bool> ShowAsync()
+    {
+        SetModalClass(HandyModalType);
+        IsVisible = true;
+        TaskCompletionSource = new TaskCompletionSource<bool>();
+        StateHasChanged();
+        return TaskCompletionSource.Task;
+    }
+
+    public virtual void Confirm()
+    {
+        IsVisible = false;
+        TaskCompletionSource?.SetResult(true);
+        StateHasChanged();
+    }
+
+    public virtual void Cancel()
+    {
+        TaskCompletionSource?.SetResult(false);
+        IsVisible = false;
+        StateHasChanged();
+    }
+
+    private void SetModalClass(HandyModalType modalType)
+    {
+        ModalClass = modalType switch
+        {
+            HandyModalType.SUCCESS => "text-success",
+            HandyModalType.WARNING => "text-warning",
+            HandyModalType.ERROR => "text-danger",
+            HandyModalType.INFO => "text-info",
+            // default to text-info
+            _ => "text-info"
+        };
+    }
+}
+
--- HandyBlazorComponents/wwwroot/handyBlazorComponents.js
+++ HandyBlazorComponents/wwwroot/handyBlazorComponents.js
@@ -1,160 +1,198 @@
 
 
 SESSION_FUNCTIONS = {
-    checkExpiry: (basePath) =>
+    checkExpiry: (dotNetObject, basePath, idleTimeOut, id, JWT_TOKEN_NAME, JWT_TOKEN_EXP_DATE) =>
+    {
+        let jwtTimeout = null;
+        let idleTimer = null;
+        let countDownInterval = null;
+        function resetAllTimers()
         {
-            // avoid checking for jwt expiring when already on the session-expired page
-            if (window.location.href.includes('session-expired'))
-            {
-                return;
-            }
-            let storedTimestamp = localStorage.getItem("expDate");
-            // console.log('storedTimestamp: ' + storedTimestamp);
-            if (storedTimestamp)
-            {
-                storedTimestamp = Number(storedTimestamp) / 1000
-                let currentTime = Math.floor(Date.now() / 1000); // Get current time in seconds
-                // console.log(basePath);
-                // console.log(currentTime - storedTimestamp);
-                if (currentTime >= storedTimestamp)
+            clearInterval(countDownInterval);
+            clearTimeout(jwtTimeout);
+            clearTimeout(idleTimer);
+        }
+
+        async function showSessionExpiredPopup(msg)
+        {
+            localStorage.removeItem(JWT_TOKEN_NAME)
+            localStorage.removeItem(JWT_TOKEN_EXP_DATE)
+            await dotNetObject.invokeMethodAsync("RedirectUser")
+            swal.fire({
+                title: "Notice",
+                html: `${msg}`,
+                icon: "info",
+                allowOutsideClick: false,
+                showCancelButton: false,
+                confirmButtonText: "Click here to start again",
+            }).then(result => {
+                if (result.isConfirmed)
                 {
-                    // console.log('session expired');
-                    // Redirect to session expired page
                     window.location.href = basePath;
                 }
-                else
-                {
-                    // Convert to milliseconds
-                    let timeLeft = (storedTimestamp - currentTime) * 1000;
-                    // console.log('timeLeft: ' + timeLeft);
-                    setTimeout(() => this.checkExpiry(basePath), timeLeft); // Re-check at expiration time
-                }
+            });
+        }
+
+        function monitorJwtToken()
+        {
+            clearTimeout(jwtTimeout)
+            // avoid checking for jwt expiring when modal is up
+            if (swal.isVisible())
+            {
+                //console.log("sweet alert pop up is open");
+                return
             }
-            else {
+            let storedTimestamp = localStorage.getItem(JWT_TOKEN_EXP_DATE);
+            //console.log('storedTimestamp: ' + storedTimestamp);
+            if (!storedTimestamp)
+            {
                 // If there's no session expiration data, recheck in 10 seconds
-                setTimeout(() => this.checkExpiry(basePath), 10000);
+                jwtTimeout = setTimeout(() => monitorJwtToken(basePath), 10000); 
+                return
             }
-        },
- 
-        startIdleTimer: (dotNetHelper, timeout) => {
-            let timer = null;
-            let countDown;
-            let idleModalWarningIsOpen = false;
- 
-            let idleTime = 0
+            storedTimestamp = Number(storedTimestamp) / 1000
+            // Get current time in seconds
+            let currentTime = Math.floor(Date.now() / 1000);
+            // console.log(basePath);
+            // console.log(currentTime - storedTimestamp);
+            if (currentTime >= storedTimestamp)
+            {
+                // console.log('session expired');
+                // Redirect to session expired page
+                resetAllTimers()
+                // console.log('token expired');
+                showSessionExpiredPopup('Your token has Expired.')
+            }
+            else
+            {
+                // Convert to milliseconds
+                let timeLeft = (storedTimestamp - currentTime) * 1000;
+                // console.log('timeLeft: ' + timeLeft);
+                jwtTimeout = setTimeout(() => monitorJwtToken(basePath), timeLeft); // Re-check at expiration time
+            }
+        }
+
+        function startIdleTimer (timeout)
+        {         
             // give one minute warning
             /*
                 timeout - warningtime = 60000
- 
+
                 formula:
                 if timeout > 60000
                     warningTime = 60000
                 else
                     warningTime = timeout - 10000
             */
- 
+
             // make sure we have at least 30 seconds to deal with
             timeout = Math.max(30000, timeout)
             // 5 minutes
             const SESSION_DISPLAY_TIME_IN_MILLISECONDS = 300000
-            let warningTime = timeout > SESSION_DISPLAY_TIME_IN_MILLISECONDS ? SESSION_DISPLAY_TIME_IN_MILLISECONDS : timeout - 10000
- 
-            function showWarning()
+            const warningTime = timeout > SESSION_DISPLAY_TIME_IN_MILLISECONDS ? SESSION_DISPLAY_TIME_IN_MILLISECONDS : timeout - 10000
+
+            function restartIdleTimer()
             {
-                let timeLeftInSeconds = warningTime / 1000;
-                // console.log('showing warning');
-                idleModalWarningIsOpen = true;
-                swal.fire({
-                    title: "Session Timeout Warning",
-                    html: `You have been idle. Redirecting in <b><span id="countdown">${timeLeftInSeconds}</span></b> seconds.`,
-                    icon: "warning",
-                    // in milliseconds
-                    timer: warningTime,
-                    allowOutsideClick: false,
-                    showCancelButton: true,
-                    confirmButtonText: "Stay Logged In",
-                    cancelButtonText: "Logout",
-                    didOpen: () => {
-                        const countdownEl = document.getElementById("countdown");
-                        countDown = setInterval(() => {
-                            timeLeftInSeconds--;
-                            //console.log(timeLeftInSeconds);
-                            countdownEl.innerText = timeLeftInSeconds;
-                            if (timeLeftInSeconds <= 0)
-                            {
-                                clearInterval(countDown);
-                                // console.log('time ran out');
-                                dotNetHelper.invokeMethodAsync("RedirectUser");
-                            }
-                        }, 1000);
-                    }
-                }).then((result) => {
-                    idleModalWarningIsOpen = false;
-                    if (result.isConfirmed) {
-                        // console.log('resetting timer');
-                        resetTimer();
-                        // dotNetHelper.invokeMethodAsync("StayLoggedIn");
-                    } else {
-                        // console.log('redirecting user');
-                        clearInterval(countDown)
-                        clearTimeout(timer);
-                        dotNetHelper.invokeMethodAsync("RedirectUser");
-                    }
-                });
-            }
- 
-            function resetTimer() {
-                clearInterval(countDown)
-                clearTimeout(timer);
-                timer = null;
-                // console.log(window.location.href);
-                // make sure to avoid firing off an idle timer when on the session-expired page (this should be the only page in the entire app that doesn't require idle timer being fired off)
-                if (window.location.href.includes('session-expired'))
+                const showWarning = async () =>
                 {
-                    return;
+                    let timeLeftInSeconds = warningTime / 1000;
+                    // console.log('showing warning');
+                    if (swal.isVisible())
+                    {
+                        resetAllTimers()
+                        return
+                    }
+                    let result = await swal.fire({
+                        title: "Session Timeout Warning",
+                        html: `You have been idle. Redirecting in <b><span id="countdown">${timeLeftInSeconds}</span></b> seconds.`,
+                        icon: "warning",
+                        // in milliseconds
+                        timer: warningTime,
+                        allowOutsideClick: false,
+                        showCancelButton: true,
+                        confirmButtonText: "Stay Logged In",
+                        cancelButtonText: "Logout",
+                        didOpen: () => {
+                            const countdownEl = document.getElementById("countdown");
+                            countDownInterval = setInterval(() => {
+                                timeLeftInSeconds--;
+                                //console.log(timeLeftInSeconds);
+                                countdownEl.innerText = timeLeftInSeconds;
+                                if (timeLeftInSeconds <= 0)
+                                {
+                                    swal.close();
+                                    resetAllTimers()
+                                    showSessionExpiredPopup('Your session has Expired.')
+                                }
+                            }, 1000);
+                        }
+                    });
+                    if (result.isConfirmed) {
+                        restartIdleTimer();
+                    } else {
+                        resetAllTimers()
+                        showSessionExpiredPopup('Your session has Expired.')
+                    }
                 }
-                idleTime = 0;
+
+                clearInterval(countDownInterval)
+                clearTimeout(idleTimer);
+                idleTimer = null;
                 // show it in (timeout - warningTime milliseconds)
                 // for example, if there's a total of 20 seconds of allowed idle time,
                 // and the warning time is 15 seconds left then the pop up will appear 5 seconds after 20 second timer has started
-                timer = setTimeout(showWarning, timeout - warningTime);
+                idleTimer = setTimeout(showWarning, timeout - warningTime);
             }
- 
+
             // Reset timer on user activity
             window.addEventListener("mousemove", () => {
                 //console.log('moved mouse');
-                // avoid resetting timer while pop up is open
-                if (!idleModalWarningIsOpen)
+                // avoid resetting timer while any of the swal pop ups are open
+                if (!swal.isVisible())
                 {
-                    resetTimer()
+                    restartIdleTimer()
                 }
             });
             window.addEventListener("keydown", () => {
                 //console.log('keydowned');
-                if (!idleModalWarningIsOpen)
+                if (!swal.isVisible())
                 {
-                    resetTimer()
+                    restartIdleTimer()
                 }
             });
             window.addEventListener("scroll", () => {
                 //console.log('scrolled');
-                if (!idleModalWarningIsOpen)
+                if (!swal.isVisible())
                 {
-                    resetTimer()
+                    restartIdleTimer()
                 }
             });
             window.addEventListener("click", () => {
                 //console.log('clicked');
-                if (!idleModalWarningIsOpen)
+                if (!swal.isVisible())
                 {
-                    resetTimer()
+                    restartIdleTimer()
                 }
             });
- 
+
             // Start the timer initially
-            resetTimer();
-        },
- 
+            restartIdleTimer();
+        }
+
+        if (!id || ![1,2].includes(Number(id)))
+        {
+            return
+        }
+        switch (Number(id))
+        {
+            case 1:
+                monitorJwtToken()
+                break
+            case 2:
+                startIdleTimer(idleTimeOut)
+                break
+        }
+    },
 }
 
 
@@ -170,29 +208,31 @@ window.dropdownInterop = {
     }
 };
 
-window.downloadFile = (base64Data, contentType, fileName) => {
-    const blob = new Blob([Uint8Array.from(atob(base64Data), c => c.charCodeAt(0))], { type: contentType });
-    const url = URL.createObjectURL(blob);
+FILE = {
+    downloadFile: (base64Data, contentType, fileName) => {
+        const blob = new Blob([Uint8Array.from(atob(base64Data), c => c.charCodeAt(0))], { type: contentType });
+        const url = URL.createObjectURL(blob);
+    
+        // Create a temporary anchor element
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = fileName;
+        document.body.appendChild(a);
+    
+        // Trigger download and clean up
+        a.click();
+        URL.revokeObjectURL(url);
+        document.body.removeChild(a);
+    },
 
-    // Create a temporary anchor element
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = fileName;
-    document.body.appendChild(a);
-
-    // Trigger download and clean up
-    a.click();
-    URL.revokeObjectURL(url);
-    document.body.removeChild(a);
-}
-
-window.saveAsFile = function(filename, fileContent)
-{
-    var link = document.createElement('a')
-    link.download = filename
-    // mime-types: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
-    link.href = 'data:text/plain;charset=utf-8,'+encodeURIComponent(fileContent)
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
+    saveAsFile: (filename, fileContent) =>
+    {
+        var link = document.createElement('a')
+        link.download = filename
+        // mime-types: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+        link.href = 'data:text/plain;charset=utf-8,'+encodeURIComponent(fileContent)
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
+    },
 }
