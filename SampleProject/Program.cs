43aba802a059291809172edba809d1fb8bdde940
centuryhopper <43508492+centuryhopper@users.noreply.github.com>
2025-03-27 17:50:20 -0400

    update

--- SampleProject/Program.cs
+++ SampleProject/Program.cs
@@ -17,7 +17,7 @@ the commands above will tell you which of your nuget packages need to be updated
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-// builder.Services.AddSingleton<HandyGridStateAbstract<HandyGridEntity, TestClass>, GridStateService>();
+builder.Services.AddSingleton<HandyGridStateAbstract<HandyGridEntity, TestClass>, GridStateService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
