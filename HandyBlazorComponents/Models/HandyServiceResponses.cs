1e46161d4fcc100f0b7491e7f61fb17b50867a8c
Leo Zhang <leo_zhang@pop-os.lan>
2026-01-07 17:30:12 -0500

    update

--- HandyBlazorComponents/Models/HandyServiceResponses.cs
+++ HandyBlazorComponents/Models/HandyServiceResponses.cs
@@ -12,6 +12,6 @@ public class HandyServiceResponses
 
     public record class HandyGeneralResponseWithPayload(bool Flag, string Message, string Payload);
 
-    public record class HandyLoginResponse(bool Flag, string Token, string Message);
+    public record class HandyLoginResponse(bool Flag, string Message, Action OnSuccess, Action OnFailure);
 }
 
